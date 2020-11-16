091d58fedfd9c931b70b1963440f16caf1069a65
Prateek Varma <prtk.varma@gmail.com>
2020-11-16 17:55:02 -0600

    made migrations for images

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -7,10 +7,10 @@ class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                             related_name='images_created',
                             on_delete=models.CASCADE)
-    title = models.CharField(max_length=200
+    title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField()
-    image = models.ImageField(upload_to='images/%Y/%m/%d/'
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True, db_index=True)
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
