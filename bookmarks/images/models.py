8c15f23e688e02e65f94d922a82175f11e1dffff
Prateek Varma <prtk.varma@gmail.com>
2020-11-16 17:44:43 -0600

    created many-to-many relationship between user likes & images

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -13,6 +13,9 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d/'
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True, db_index=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
